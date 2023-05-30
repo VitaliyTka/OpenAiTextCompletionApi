7688cd6c6d22b5d45be2bdb8d632170d182ccd19
VitaliyTka <vtkp72@gmail.com>
2023-05-30 10:25:26 +0300

    Add project files.

---
+++ OpenAiTextCompletionApi/Data/ChatOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenAiTextCompletionApi.Data
+{
+    public class ChatOptions
+    {
+        public string ApiKey { get; set; } = "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";
+        public string ApiUrl { get; set; } = "https://api.openai.com/v1/chat/completions";
+        public string GtpModel { get; set; } = "gpt-3.5-turbo";
+    }
+}
---
+++ OpenAiTextCompletionApi/Services/OpenAIService.cs
@@ -0,0 +1,28 @@
+﻿using OpenAiTextCompletionApi.Models;
+using OpenAiTextCompletionApi.Data;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace OpenAiTextCompletionApi.Services
+{
+    public class OpenAIService
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly ChatOptions _options = new ChatOptions();
+
+
+        public async Task<Message> CreateChatCompletion(List<Message> messages)
+        {
+            var request = new { model = _options.GtpModel, messages = messages.ToArray() };
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _options.ApiKey);
+
+            var response = await _httpClient.PostAsJsonAsync(_options.ApiUrl, request);
+            response.EnsureSuccessStatusCode();
+
+            var chatCompletionResponse = await response.Content.ReadFromJsonAsync<ChatbotResponse>();
+            return chatCompletionResponse?.choices.First().message;
+        }
+    }
+}
